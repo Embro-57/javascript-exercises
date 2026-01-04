c8ac78f742417a5f80bbd2d3bb15f6dace763e8d
Embro-57 <250835748+Embro-57@users.noreply.github.com>
2026-01-04 14:50:26 +0100

    Several Odin foundations js assignments done
    Exersices 01 to 06 done for javascript.

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,4 +1,15 @@
-const repeatString = function() {
+
+const repeatString = function(singleString, n) {
+    if (n < 0) {
+        return "ERROR";
+    }
+    else {
+        let finalString = ""
+        for (let i = 0; i < n; i++) {
+            finalString = finalString + singleString;
+        };
+        return finalString;
+    }
 
 };
 
